51ef066878c0ee02fe63142de91b5e4e0462ccff
Lim Yixiang <limyixiang2@gmail.com>
2024-12-10 21:23:28 +0800

    Refactor database connection to database.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,37 +1,17 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	// "log"
-	// "net/http"
-	"os"
+	// "fmt"
+	"log"
 
-	// "github.com/CVWO/sample-go-app/internal/router"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/CVWO/sample-go-app/internal/database"
+	// _ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	// r := router.Setup()
-	// fmt.Print("Listening on port 8000 at http://localhost:8000!")
-
-	// log.Fatalln(http.ListenAndServe(":8000", r))
-
-	fmt.Println(os.Getenv("MYSQL_PASSWORD"))
-	password := os.Getenv("MYSQL_PASSWORD")
-	db, err := sql.Open("mysql", "root:"+password+"@tcp(localhost:3306)/testdb")
-
-	if err != nil {
-		fmt.Println("Error validating sql.Open arguments")
-		panic(err.Error())
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("Error verifying connection to database with db.Ping")
-		panic(err.Error())
-	}
-
-	fmt.Println("Successfully connected to database!")
+	db, err := database.GetDB()
+    if err != nil {
+        log.Fatalf("Failed to connect to database: %v", err)
+    }
+    defer db.Close()	
 }
--- internal/database/database.go
+++ internal/database/database.go
@@ -1,8 +1,33 @@
 package database
 
+import (
+    "database/sql"
+    "fmt"
+    "os"
+
+    _ "github.com/go-sql-driver/mysql"
+)
+
 type Database struct {
+    *sql.DB
 }
 
 func GetDB() (*Database, error) {
-	return &Database{}, nil
+    password := os.Getenv("MYSQL_PASSWORD")
+    if password == "" {
+        return nil, fmt.Errorf("MYSQL_PASSWORD environment variable is not set")
+    }
+
+    db, err := sql.Open("mysql", "root:"+password+"@tcp(localhost:3306)/testdb")
+    if err != nil {
+        return nil, fmt.Errorf("error validating sql.Open arguments: %w", err)
+    }
+
+    err = db.Ping()
+    if err != nil {
+        return nil, fmt.Errorf("error verifying connection to database with db.Ping: %w", err)
+    }
+
+    fmt.Println("Successfully connected to database!")
+    return &Database{DB: db}, nil
 }
