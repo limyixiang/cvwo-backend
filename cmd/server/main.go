04f13da042db327806d4f755aa10a74d348fcd41
Lim Yixiang <limyixiang2@gmail.com>
2024-12-10 18:23:39 +0800

    Set up MySQL test database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"log"
-	"net/http"
+	// "log"
+	// "net/http"
+	"os"
 
-	"github.com/CVWO/sample-go-app/internal/router"
+	// "github.com/CVWO/sample-go-app/internal/router"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	r := router.Setup()
-	fmt.Print("Listening on port 8000 at http://localhost:8000!")
+	// r := router.Setup()
+	// fmt.Print("Listening on port 8000 at http://localhost:8000!")
 
-	log.Fatalln(http.ListenAndServe(":8000", r))
+	// log.Fatalln(http.ListenAndServe(":8000", r))
+
+	fmt.Println(os.Getenv("MYSQL_PASSWORD"))
+	password := os.Getenv("MYSQL_PASSWORD")
+	db, err := sql.Open("mysql", "root:"+password+"@tcp(localhost:3306)/testdb")
+
+	if err != nil {
+		fmt.Println("Error validating sql.Open arguments")
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error verifying connection to database with db.Ping")
+		panic(err.Error())
+	}
+
+	fmt.Println("Successfully connected to database!")
 }
--- go.mod
+++ go.mod
@@ -6,3 +6,8 @@ require (
 	github.com/go-chi/chi/v5 v5.0.10
 	github.com/pkg/errors v0.9.1
 )
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+)
