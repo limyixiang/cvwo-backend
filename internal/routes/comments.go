43276a0c284b3f33ee6ff65959a172f8b232e0f7
Lim Yixiang <limyixiang2@gmail.com>
2024-12-25 17:02:41 +0800

    Update CommentsRoutes

--- internal/routes/comments.go
+++ internal/routes/comments.go
@@ -15,7 +15,7 @@ func CommentsRoutes() http.Handler {
     r.Get("/post/{postID}", comments.HandleList)  // List all comments by post ID
     r.Get("/{id}", comments.HandleGet)       // Get a specific comment by ID
     r.Post("/", comments.HandleCreate)       // Create a new comment
-    r.Put("/{id}", comments.HandleUpdate)    // Update a specific comment by ID
+    r.Patch("/{id}", comments.HandleUpdate)    // Update a specific comment by ID
     r.Delete("/{id}", comments.HandleDelete) // Delete a specific comment by ID
 
     return r
