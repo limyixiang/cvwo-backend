8a004348fc633ea2832a3a12a05f57786eaf87e0
Lim Yixiang <limyixiang2@gmail.com>
2024-12-26 23:37:35 +0800

    Fix bug with duplicate post patch routes

--- internal/routes/posts.go
+++ internal/routes/posts.go
@@ -15,7 +15,7 @@ func PostsRoutes() http.Handler {
     r.Get("/", posts.HandleList)                                // List all posts
     r.Get("/category/{id}", posts.HandleListByCategory)         // List all posts in a specific category
     r.Get("/{id}", posts.HandleGet)                             // Get a specific post by ID
-    r.Patch("/{id}", posts.HandleUpdateLastUpdated)             // Update the time of a specific post by ID
+    r.Patch("/{id}/updatetime", posts.HandleUpdateLastUpdated)             // Update the time of a specific post by ID
     r.Post("/", posts.HandleCreate)                             // Create a new post
     r.Patch("/{id}", posts.HandleUpdate)                          // Update a specific post by ID
     r.Delete("/{id}", posts.HandleDelete)                       // Delete a specific post by ID
